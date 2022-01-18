810221053b451ccb14e7ad8eaab1c75818ab3126
jdm79 <james.malvern@gmail.com>
2022-01-18 21:43:52 +0100

    refactor parent elements, add datetime

--- index.js
+++ index.js
@@ -3,18 +3,24 @@ const cheerio = require("cheerio");
 
 const scrapingResults = []
 
- async function main() {
-     const browser = await puppeteer.launch({ headless: false });
-     const page = await browser.newPage();
-     await page.goto(
-         "https://sfbay.craigslist.org/d/software-qa-dba-etc/search/sof"
-         );
-     const html = await page.content();
-     const $ = cheerio.load(html); 
-     $(".result-title").each((index, element) => console.log($(element).text()));
+async function main() {
+    const browser = await puppeteer.launch({ headless: false });
+    const page = await browser.newPage();
+    await page.goto(
+        "https://sfbay.craigslist.org/d/software-qa-dba-etc/search/sof"
+        );
+    const html = await page.content();
+    const $ = cheerio.load(html); 
 
- };
+    const results = $(".result-info").map((index, element) => {
+        const titleElement = $(element).find(".result-title");
+        const timeElement = $(element).find(".result-date");
+        const title = $(titleElement).text();
+        const url = $(titleElement).attr("href");
+        const datePosted = new Date($(timeElement).attr("datetime"));
+        return { title, url, datePosted };
+    }).get();
+    console.log(results);
+};
 
  main()
-
-//  "result-title hdrlnk"
